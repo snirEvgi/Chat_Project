9a717653703760e48d7df8ca2e8cb34edd1c5ac0
Yuval Chen <pinewater700@gmail.com>
2023-11-09 18:50:32 +0200

    new-client

--- socket-client/src/App.tsx
+++ socket-client/src/App.tsx
@@ -1,34 +1,76 @@
 import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
 
-// import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import SocketC from "./Socket";
-import { Link } from "react-router-dom";
-import Products from "./UseDefferedValue";
+const socket = io("ws://localhost:4300");
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [message, setMessage] = useState("");
+  const [chatRows, setChatRows] = useState<any[]>([]);
+
+  useEffect(() => {
+    socket.on("message-from-server", (message) => {
+      setChatRows((prevChatRows) => [...prevChatRows, message]);
+    });
+
+    socket.on("new-user-logged-in", (message) => {
+      setChatRows((prevChatRows) => [
+        ...prevChatRows,
+        { user: "Server", message },
+      ]);
+    });
+  }, []);
+
   return (
-    <div>
-      <h1> Main </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Socket</Link>
-          </li>
-          <li>
-            <Link to="/usedefferedvalue">UseDefferedValue</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<SocketC />} />
-        <Route path="/usedefferedvalue" element={<Products />} />
-        <Route path="/dashboard" element={<div>Dashboard</div>} />
-      </Routes>
-    </div>
+    <>
+      <div className="card">
+        <button
+          onClick={() => {
+            socket.emit("client-login", userName);
+          }}
+        >
+          Login
+        </button>
+        <input
+          type="text"
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+      </div>
+
+      <div className="card">
+        <button
+          onClick={() => {
+            socket.emit("client-send-message", message);
+          }}
+        >
+          Start Chat
+        </button>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+      </div>
+
+      <div>
+        {chatRows.map((row, index) => {
+          const isCurrentUser = row.user === userName;
+          console.log(row);
+
+          return (
+            <h3 key={index} style={{ color: isCurrentUser ? "green" : "red" }}>
+              {`${row.user}: ${row.message}`}
+            </h3>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
