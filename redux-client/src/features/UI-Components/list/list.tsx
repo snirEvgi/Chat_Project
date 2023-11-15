a6e7073656d6775aa821fb9c3296d8ac8c9f8fb7
Yuval Chen <pinewater700@gmail.com>
2023-11-15 15:59:11 +0200

    git-fix

--- redux-client/src/features/UI-Components/list/list.tsx
+++ redux-client/src/features/UI-Components/list/list.tsx
@@ -1,24 +1,47 @@
-import React, { SyntheticEvent } from "react"
+import React, { useState } from "react"
 import { Panel } from "primereact/panel"
 import { ScrollPanel } from "primereact/scrollpanel"
 import "./list.css"
+import SingleChatComponent from "../../pages/singleChat"
 
 const List = (props: any) => {
-const handler = async ()=>{
-    
-}
+  const [selectedChatId, setSelectedChatId] = useState<number | null>(null)
+
+  const chatHandler = (chatId: number) => {
+    console.log(chatId)
+    setSelectedChatId((prevChatId) => (prevChatId === chatId ? null : chatId))
+  }
+
   return (
-    <Panel className="listContainer" header="Chat List">
-      <ScrollPanel style={{ height: "20rem" }}>
-        <ul onClick={props.onClick} className="p-list p-list-bordered chatList">
-          {props.chats.map((chat: any) => (
-            <li key={chat.chatId} className="p-list-item chatItem">
-              <span className="chatName">{`${chat.firstUserId} & ${chat.secondUserId}`}</span>
-            </li>
-          ))}
-        </ul>
-      </ScrollPanel>
-    </Panel>
+    <div className="list-container">
+      <Panel className="listContainer" header="Chat List">
+        <ScrollPanel style={{ height: "20rem" }}>
+          <ul
+            onClick={props.onClick}
+            className="p-list p-list-bordered chatList"
+          >
+            {props.chats.map((chat: any) => (
+              <li key={chat.chatId} className="p-list-item chatItem">
+                <span
+                  onClick={() => {
+                    chatHandler(chat.chatId)
+                  }}
+                  className="chatName"
+                >{`${chat.firstUserId} & ${chat.secondUserId}`}</span>
+              </li>
+            ))}
+          </ul>
+        </ScrollPanel>
+      </Panel>
+      {selectedChatId !== null && (
+        <div className="rightSideContainer">
+          <SingleChatComponent
+            chatOn={true}
+            roomId={selectedChatId}
+          ></SingleChatComponent>
+        </div>
+      )}
+    </div>
   )
 }
 
--- redux-client/src/features/pages/singleChat/singleChatAPI.ts
+++ redux-client/src/features/pages/singleChat/singleChatAPI.ts
@@ -1,18 +1,17 @@
 import axios from "axios"
-import { url } from "../sign-up/signUpAPI";
-import { IMessage } from "./singleChatSlice";
+import { url } from "../sign-up/signUpAPI"
+import { IMessage } from "./singleChatSlice"
 
-export async function postNewMessage(message:IMessage) {
+export async function postNewMessage(message: IMessage) {
   try {
-    const result = await axios.post(`${url}/messages/newMessage`,message)
-    console.log(result);
-    
+    const result = await axios.post(`${url}/messages/newMessage`, message)
+    console.log(result)
+
     return result.data
   } catch (error) {
-  console.log(error);
+    console.log(error)
   }
 }
-
 
 export async function getMessages(chatId: number) {
   try {
