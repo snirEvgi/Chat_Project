b1099d91a06ee411eb9cbabb8de702763c9a2a58
Yuval Chen <pinewater700@gmail.com>
2023-11-26 05:34:46 +0200

    groupChat

--- redux-client/src/features/UI-Components/list/list.tsx
+++ redux-client/src/features/UI-Components/list/list.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Panel } from "primereact/panel"
 import { ScrollPanel } from "primereact/scrollpanel"
 import "./list.css"
@@ -11,19 +11,35 @@ import {
   fetchChatsBySecondId,
 } from "../../pages/homePage/mainAPI"
 import { Socket, io } from "socket.io-client"
+import GroupChatModal from "../groupChatModal"
 
 const List = (props: any) => {
   const [selectedChatId, setSelectedChatId] = useState<number | null>(null)
   const [currentChat, setCurrentChat] = useState<Array<any>>([])
   const [isOn, setIsOn] = useState<boolean>(false)
+  const [isGroupOn, setIsGroupOn] = useState<boolean>(false)
   const [chats, setChats] = useState<Array<any>>([])
   const userData = JSON.parse(localStorage.getItem("userRecord") as any)
   const token = localStorage.getItem("token")
   const [socket, setSocket] = useState<Socket>()
   const [isMessageNew, setIsMessageNew] = useState<boolean>(false)
-  
-  // console.log("sadasdasdsad",currentChat);
-  
+  const [showGroupChatModal, setShowGroupChatModal] = useState<boolean>(false)
+  const groupChatNameRef = useRef<HTMLInputElement>(null)
+
+  const handleGroupChatIconClick = () => {
+    setShowGroupChatModal(true)
+  }
+
+  const handleCreateGroupChat = () => {
+    setShowGroupChatModal(true)
+    const groupChatName = groupChatNameRef.current?.value
+    // Perform any validations or checks
+    // ...
+
+    // Close the modal
+    setShowGroupChatModal(false)
+  }
+
   const userName = `${userData?.firstName} ${userData?.lastName}`
   const onlineUsers =
     JSON.parse(localStorage.getItem("onlineUsers") as any) || []
@@ -55,7 +71,9 @@ const List = (props: any) => {
   //   }) || []
 
   const chatHandler = (chat: any) => {
-    setSelectedChatId((prevChatId) => (prevChatId === chat.chatId ? null : chat.chatId))
+    setSelectedChatId((prevChatId) =>
+      prevChatId === chat.chatId ? null : chat.chatId,
+    )
     setCurrentChat(chat)
   }
 
@@ -70,22 +88,19 @@ const List = (props: any) => {
   useEffect(() => {
     fetchChats()
   }, [])
- 
-  
+
   useEffect(() => {
     if (token) {
       const newSocket = io("http://localhost:4300")
       setSocket(newSocket)
-      
+
       newSocket.on("connect", () => {
         newSocket.emit("user-logged-in", userData)
       })
       socket?.on("message", (data) => {
-        console.log(data,"isMsaljdlasjdsljdjsal");
-       
+        console.log(data, "isMsaljdlasjdsljdjsal")
+
         setIsMessageNew(data.name !== userName && data.isNew)
-      
-      
       })
       return () => {
         newSocket.disconnect()
@@ -100,6 +115,11 @@ const List = (props: any) => {
     } catch (error) {
       console.error("Error fetching chats:", error)
     }
+  }
+
+  const handleGroupOnClose = () => {
+    setShowGroupChatModal(false)
+    setIsGroupOn(false)
   }
 
   const createNewChatHandler = async (user: any) => {
@@ -117,50 +137,75 @@ const List = (props: any) => {
   return (
     <div className="">
       {selectedChatId !== null && (
-        <SingleChatComponent chatOn={true} currentChat={currentChat} roomId={selectedChatId} />
+        <SingleChatComponent
+          chatOn={true}
+          currentChat={currentChat}
+          roomId={selectedChatId}
+        />
       )}
       <div className="flex flex-row h-screen w-1/6 bg-gray-900 absolute top-12 left-0">
         <ul className="list-none mt-4 w-full overflow-y-auto p-2">
+          <li className=" mb-2 p-3  bg-gray-800 rounded-2xl ">
+            <span
+              onClick={() => {
+                setIsOn(!isOn)
+                setIsGroupOn(false)
+              }}
+              className="cursor-pointer mx-2 border-[2px] hover:bg-gray-600 border-teal-400 rounded-3xl font-bold p-2 text-teal-400"
+            >
+              <i className="pi pi-plus"></i>
+            </span>
+            <span
+              onClick={() => {
+                setIsGroupOn(!isGroupOn)
+                setIsOn(false)
+                setShowGroupChatModal(true)
+              }}
+              className="cursor-pointer mx-2 border-[2px] hover:bg-gray-600 border-teal-400 rounded-3xl font-bold p-2 text-teal-400"
+            >
+              <i className="pi pi-users"></i>
+            </span>
+          </li>
+          <div>
+            {isOn && (
+              <ul className="list-none mt-4 w-full overflow-y-auto p-2">
+                {filteredUsersData.length > 0 ? (
+                  filteredUsersData.map((user: any, index: number) => (
+                    <div
+                      key={user?.email}
+                      className=""
+                      onClick={() => createNewChatHandler(user)}
+                    >
+                      <li
+                        key={user.id}
+                        className="cursor-pointer mb-2 p-3  bg-gray-800 rounded-2xl"
+                      >
+                        <span className=" hover:bg-gray-600 border-teal-400 rounded-3xl font-bold  text-teal-400">
+                          {` ${user.firstName} ${user.lastName}`}{" "}
+                          {isUserOnline[index] ? "Online" : "Offline"}
+                        </span>
+                      </li>
+                    </div>
+                  ))
+                ) : (
+                  <li>
+                    <span>No available users</span>
+                  </li>
+                )}
+              </ul>
+            )}
 
-          <li   className=" mb-2 p-3  bg-gray-800 rounded-2xl ">
-
-         <span onClick={() => {
-            setIsOn(!isOn)
-          }} className="cursor-pointer mx-2 border-[2px] hover:bg-gray-600 border-teal-400 rounded-3xl font-bold p-2 text-teal-400">  
-          <i className="pi pi-plus"></i></span>
-         <span onClick={() => {
-            setIsOn(!isOn)
-          }} className="cursor-pointer mx-2 border-[2px] hover:bg-gray-600 border-teal-400 rounded-3xl font-bold p-2 text-teal-400">  
-          <i className="pi pi-users"></i></span>
-        </li>
-        <div > 
-
-        {isOn && (
-           <ul className="list-none mt-4 w-full overflow-y-auto p-2">
-           {filteredUsersData.length > 0 ? (
-             filteredUsersData.map((user: any, index: number) => (
-               <div
-                 key={user?.email}
-                 className=""
-                 onClick={() => createNewChatHandler(user)}
-               >
-                 <li key={user.id} className="cursor-pointer mb-2 p-3  bg-gray-800 rounded-2xl">
-                   <span className=" hover:bg-gray-600 border-teal-400 rounded-3xl font-bold  text-teal-400">
-                     {` ${user.firstName} ${user.lastName}`}{" "}
-                     {isUserOnline[index] ? "Online" : "Offline"}
-                   </span>
-                 </li>
-               </div>
-             ))
-           ) : (
-             <li>
-               <span>No available users</span>
-             </li>
-           )}
-         </ul>
-        )}
-
-        </div>
+            {isGroupOn && (
+              <ul className="list-none mt-4 w-full overflow-y-auto p-2">
+                <GroupChatModal
+                  visible={showGroupChatModal}
+                  onClose={handleGroupOnClose}
+                  onCreate={handleCreateGroupChat}
+                />
+                <div className="mt-4">Select Group :</div>
+              </ul>
+            )}
+          </div>
           {chats.map((chat: any) => (
             <li
               key={chat.chatId}
--- redux-client/src/main.tsx
+++ redux-client/src/main.tsx
@@ -2,17 +2,19 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
+import { ChakraProvider } from "@chakra-ui/react"
 import App from "./App"
 // index.js or index.tsx
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+import "primereact/resources/themes/saga-blue/theme.css"
+import "primereact/resources/primereact.min.css"
+import "primeicons/primeicons.css"
+import "primeflex/primeflex.css"
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <Provider store={store}>
-    <App />
-    </Provider>
+  <Provider store={store}>
+    {/* <ChakraProvider> */}
+      <App />
+    {/* </ChakraProvider> */}
+  </Provider>,
 )
